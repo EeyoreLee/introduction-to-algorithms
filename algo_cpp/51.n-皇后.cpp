5b17349a51b70913095a6ddc2d42ec01df55d648
EeyoreLee <li.chunyu0412@outlook.com>
2022-06-20 18:34:47 +0800

    update

--- algo_cpp/51.n-皇后.cpp
+++ algo_cpp/51.n-皇后.cpp
@@ -5,24 +5,57 @@
  */
 
 // @lc code=start
+#include <string>
 class Solution {
 public:
     vector<vector<string>> solveNQueens(int n) {
-        vector<string> board(n, string(n, "."));
+        vector<string> board(n, string(n, '.'));
         vector<vector<string>> res;
         int row = 0;
-        backtrace(board, row, res, n);
+        int col = 0;
+        backtrace(board, res, row, n);
         return res;
     }
-    void backtrace(vector<string>& board, int row, vector<vector<string>>& res, int n) {
-        for(auto col: n){
-            if (isValid(board, col, row)) {
-                
+    void backtrace(vector<string>& board, vector<vector<string>>& res, int row, int n) {
+        if (row == board.size()){
+            res.push_back(board);
+            return;
+        }
+        for(int col=0; col<n; col++){
+            if (isValid(board, row, col)) {
+                board[row][col] = 'Q';
+                backtrace(board, res, row+1, n);
+                board[row][col] = '.';
+            }
+            else{
+                continue;
             }
         }
+        return;
     }
-    bool isValid(vector<string>& board, int col, int row) {
-        return false;
+    bool isValid(vector<string>& board, int row, int col) {
+        int width = board.size();
+        for (int i=0; i<width; i++) {
+            if (board[row][i]=='Q') {
+                return false;
+            }
+        }
+        for (int i=row; i>=0; i--) {
+            if (board[i][col]=='Q') {
+                return false;
+            }
+        }
+        for (int i=row, j=col; i>=0&&j>=0; i--, j--) {
+            if (board[i][j]=='Q') {
+                return false;
+            }
+        }
+        for (int i=row, j=col; i>=0&&j<width; i--, j++) {
+            if (board[i][j]=='Q') {
+                return false;
+            }
+        }
+        return true;
     }
 };
 // @lc code=end
