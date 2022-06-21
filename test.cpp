6b8f377ea56ada62707ec9c76e9e4d3a45a1a740
EeyoreLee <li.chunyu0412@outlook.com>
2022-06-21 14:12:11 +0800

    update

--- test.cpp
+++ test.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using namespace std;
 
 int main(){
-    int* x = (int*)malloc(4);
+    int n = 5;
+    int row = 2;
+    int col = 3;
+    string s = "...";
+    s[1] = "Q";
+    vector<string> board(n, string(n, '.'));
+    board[row] = "..Q..";
+    for (auto c: board){
+        cout<< c <<endl;
+    }
     return 0;
 }
