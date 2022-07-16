9be964225ba65149bcf64f26a1abd4a463aaf0c1
EeyoreLee <li.chunyu0412@outlook.com>
2022-07-16 18:06:18 +0800

    309

---
+++ leetcode_cpp/309.最佳买卖股票时机含冷冻期.cpp
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=309 lang=cpp
+ *
+ * [309] 最佳买卖股票时机含冷冻期
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        vector<vector<int>> dp_table(prices.size(), vector<int>(2,0));
+        for (int i=0; i<prices.size(); i++) {
+            if (i==0) {
+                dp_table[i][0] = 0;
+                dp_table[i][1] = -prices[i];
+                continue;
+            }
+            if(i==1) {
+                dp_table[i][0] = max<int>(dp_table[i-1][0], dp_table[i-1][1]+prices[i]);
+                dp_table[i][1] = max<int>(dp_table[i-1][1], dp_table[i-1][0]-prices[i]);
+                continue;
+            }
+            dp_table[i][0] = max<int>(dp_table[i-1][0], dp_table[i-1][1]+prices[i]);
+            dp_table[i][1] = max<int>(dp_table[i-1][1], dp_table[i-2][0]-prices[i]);
+        }
+        return dp_table[prices.size()-1][0];
+    }
+};
+// @lc code=end
+
