30ed40c0f70d8b9fa741c832c672ef8ecb3844b0
EeyoreLee <li.chunyu0412@outlook.com>
2022-07-18 11:28:25 +0800

    123

--- leetcode_cpp/123.买卖股票的最佳时机-iii.cpp
+++ leetcode_cpp/123.买卖股票的最佳时机-iii.cpp
@@ -8,19 +8,19 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        vector<vector<vector<int>>> dp_table(prices.size(), vector<vector<int>>(2,vector<int>(2,INT_MIN)));
+        vector<vector<vector<int>>> dp_table(prices.size(), vector<vector<int>>(3,vector<int>(2,0)));
         for (int i=0; i<prices.size(); i++) {
-            if (i==0){
-                dp_table[i][0][0] = 0;
-                dp_table[i][1][1] = -prices[i];
-                continue;
-            }
-            for (int k=0; k<2; k++){
+            for (int k=1; k<3; k++){
+                if (i==0) {
+                    dp_table[i][k][0] = 0;
+                    dp_table[i][k][1] = -prices[i];
+                    continue;
+                }
                 dp_table[i][k][0] = max<int>(dp_table[i-1][k][0], dp_table[i-1][k][1]+prices[i]);
                 dp_table[i][k][1] = max<int>(dp_table[i-1][k][1], dp_table[i-1][k-1][0]-prices[i]);
             }
         }
-        return dp_table[prices.size()-1][0][0];
+        return dp_table[prices.size()-1][2][0];
     }
 };
 // @lc code=end
